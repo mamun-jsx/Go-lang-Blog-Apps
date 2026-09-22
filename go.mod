87f4a61c7b1300bf02ea10a11e5fc2e50f762de3
almamun528 <ayapths@gmail.com>
2026-09-22 21:27:28 +0600

    write models and relations

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mamun-jsx/Go-lang-Blog-Apps
 
 go 1.27.1
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+)
---
+++ internal/models/user.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID          uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
+	UserName    string    `gorm:"unique;not null" json:"user_name"`
+	Email       string    `gorm:"unique;not null" json:"email"`
+	Password    string    `gorm:"not null" json:"-"`
+	DisplayName string    `json:"display_name"`
+	Bio         string    `json:"bio"`
+	Role        string    `gorm:"default:reader" json:"role"`
+	IsActive    bool      `gorm:"default:true" json:"is_active"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+
+	// ? relations
+
+	// relation with post
+
+	Posts    []Post     `gorm:"foreignKey:AuthorID"json:"posts,omitempty"`
+	Comments []Comment  `gorm:"foreignKey:UserId" json:"comment"`
+	Reacts   []Reaction `gorm"foreignKey:UserID"json:"reacts"`
+}
